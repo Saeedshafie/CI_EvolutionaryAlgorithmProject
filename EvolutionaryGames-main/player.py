cf209f28110d14df7b0031632a32b0ee7bc59f93
Saeed Shafie <saeedshafie@aut.ac.ir>
2023-01-16 20:56:23 +0330

    input vector created and trained

--- EvolutionaryGames-main/player.py
+++ EvolutionaryGames-main/player.py
@@ -87,6 +87,7 @@ class Player():
     def init_network(self, mode):
 
         # you can change the parameters below
+        # Probably No Change is Needed
 
         layer_sizes = None
         if mode == 'gravity':
@@ -99,15 +100,54 @@ class Player():
 
     
     def think(self, mode, box_lists, agent_position, velocity):
-
-        # TODO
+        import math
+    
+        # In This Function we Need to Create the Input Vector that we Want to Give to Our NN.
+        # This Happens by FeedForwarding the Surrounding Data to NN through The Vector.
+        
+        # Given Examples:
         # mode example: 'helicopter'
         # box_lists: an array of `BoxList` objects
         # agent_position example: [600, 250]
         # velocity example: 7
+        
 
-        direction = -1
-        return direction
+        # Cause the box_lists can be Empty in the Begining, We need a Counter
+        box_Counter = 0
+        while mode == 'helicopter' or mode == 'gravity':
+            if len(box_lists):
+                if box_lists[box_Counter].x > agent_position[0]:                    # If the the Box is Ahead(Not Passed)
+                    x0 = agent_position[0] - box_lists[box_Counter].x               # Horizantal Distance
+                    y0 = agent_position[1] - box_lists[box_Counter].gap_mid         # Vertical Distance(Gap!)
+                    if len(box_lists) > 1:                                          # More Boxes than 1 box is Ahead
+                        x1 = agent_position[0] - box_lists[box_Counter + 1].x       # Horizantal Distance    
+                        y1 = agent_position[1] - box_lists[box_Counter + 1].gap_mid # Vertical Distance(Gap!)
+                    else:
+                        x1 = box_lists[box_Counter].x + 200                         # Might Not Need This Else
+                        y1 = y0
+                    break
+                else:
+                    box_Counter += 1
+            else:                                       # Default Status Where box_lists is EMPTY!
+                x0 = x1 = 1280                          
+                y0 = y1 = 360
+                break
+        # Create the Input Vector and NORMALIZE!
+        # Divided to There Maximum Possible Value to Be Normalized
+        Input_Vector = [[velocity / 10],
+                        [math.sqrt((x0 ** 2) + (y0 ** 2)) / 1468.6],
+                        [x0 / 1280],
+                        [y0 / 720],
+                        [x1 / 1280],
+                        [y1 / 720]]
+        output = self.nn.forward(Input_Vector) # Passing the InputVector to our FeedForward Network
+        # Decide the Direction
+        if output > 0.5:
+            return 1        # This is Direction
+        else:
+            return -1
+
+        # Thrust Creation if Time    
 
     def collision_detection(self, mode, box_lists, camera):
         if mode == 'helicopter':
